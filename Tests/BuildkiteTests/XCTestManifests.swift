44fde33a2125a1dbd54bbce0edb1b4dda1ac06d0
Aaron Sky <aaronsky@skyaaron.com>
2020-05-19 01:14:00 -0400

    Removed disabled tests that are still not supported on Linux yet

--- Tests/BuildkiteTests/XCTestManifests.swift
+++ Tests/BuildkiteTests/XCTestManifests.swift
@@ -121,8 +121,6 @@ extension JobsTests {
     static let __allTests__JobsTests = [
         ("testJobsDeleteLogOutput", testJobsDeleteLogOutput),
         ("testJobsEnvironmentVariables", testJobsEnvironmentVariables),
-        ("testJobsLogOutputAlternativeHTML", testJobsLogOutputAlternativeHTML),
-        ("testJobsLogOutputAlternativePlainText", testJobsLogOutputAlternativePlainText),
         ("testJobsLogOutput", testJobsLogOutput),
         ("testJobsRetry", testJobsRetry),
         ("testJobsUnblock", testJobsUnblock),
